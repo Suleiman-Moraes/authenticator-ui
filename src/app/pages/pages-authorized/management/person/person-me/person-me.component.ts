8b613d107291011ab8b6475cf6e59f7c6868d796
suleiman-moraes <suleimanmoraes@gmail.com>
2024-02-13 00:06:43 +0000

    change css

--- src/app/pages/pages-authorized/management/person/person-me/person-me.component.ts
+++ src/app/pages/pages-authorized/management/person/person-me/person-me.component.ts
@@ -1,15 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { CardModule } from 'primeng/card';
+import { ToastModule } from 'primeng/toast';
 import { BaseResourceUtilComponent } from 'src/app/shared/components/base-resource-util/base-resource-util.component';
 import { PersonDTO } from 'src/app/shared/model/person/person-dto.model';
 import { PersonMeService } from 'src/app/shared/service/person-me.service';
+import { PersonFormRecycleComponent } from '../person-form-recycle/person-form-recycle.component';
 
 @Component({
     selector: 'app-person-me',
     standalone: true,
     imports: [
-        CommonModule
+        CommonModule,
+        CardModule,
+        ToastModule,
+        PersonFormRecycleComponent
     ],
     templateUrl: './person-me.component.html',
     styleUrl: './person-me.component.scss'
