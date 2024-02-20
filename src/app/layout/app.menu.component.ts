425fe6cbb1e76adc20a3cd7bfaee4d46793a205a
suleiman-moraes <suleimanmoraes@gmail.com>
2024-02-20 20:41:26 +0000

    add dynamic menu

--- src/app/layout/app.menu.component.ts
+++ src/app/layout/app.menu.component.ts
@@ -1,30 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuService } from '../shared/service/menu.service';
 import { LayoutService } from './service/app.layout.service';
 
 @Component({
-  selector: 'app-menu',
-  templateUrl: './app.menu.component.html'
+    selector: 'app-menu',
+    templateUrl: './app.menu.component.html'
 })
 export class AppMenuComponent implements OnInit {
 
-  model: any[] = [];
+    model: any[] = [];
 
-  constructor(public layoutService: LayoutService) { }
+    constructor(
+        public layoutService: LayoutService,
+        private menuService: MenuService
+    ) { }
 
-  ngOnInit() {
-    this.model = [
-      {
-        label: 'Home',
-        items: [
-          { label: 'Dashboard', icon: 'pi pi-fw pi-home', routerLink: ['/pages'] }
-        ]
-      },
-      {
-        label: 'Administração',
-        items: [
-          { label: 'Pessoas', icon: 'pi pi-fw pi-id-card', routerLink: ['/pages/person'] }
-        ]
-      }
-    ];
-  }
+    ngOnInit() {
+        this.model = [
+            {
+                label: 'Home',
+                items: [
+                    { label: 'Dashboard', icon: 'pi pi-fw pi-home', routerLink: ['/pages'] }
+                ]
+            }
+        ];
+        this.menuService.findAll().subscribe({
+            next: (res) => {
+                this.model = res;
+            }
+        });
+    }
 }
